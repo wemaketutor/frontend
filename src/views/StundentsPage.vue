ddc97370a7e55e8c058387ad7c6ece88e5d3bdb8
McMaxim <94379675+McMaxim@users.noreply.github.com>
2024-12-12 15:38:16 +0300

    Update StundentsPage.vue

--- src/views/StundentsPage.vue
+++ src/views/StundentsPage.vue
@@ -4,7 +4,40 @@
 
 <template>
 
-
+  <div class="container">
+    <div class="d-flex">
+      <div class="row">
+        <div class="col-3">Student1</div>
+        <div class="col-3">12</div>
+        <div class="col-3">2500</div>
+        <div class="col-3">365</div>
+      </div>
+      <div class="row">
+        <div class="col-3">Student2</div>
+        <div class="col-3">12</div>
+        <div class="col-3">1500</div>
+        <div class="col-3">360</div>
+      </div>
+      <div class="row">
+        <div class="col-3">Student3</div>
+        <div class="col-3">52</div>
+        <div class="col-3">11500</div>
+        <div class="col-3">325</div>
+      </div>
+      <div class="row">
+        <div class="col-3">Student4</div>
+        <div class="col-3">16</div>
+        <div class="col-3">2250</div>
+        <div class="col-3">200</div>
+      </div>
+      <div class="row">
+        <div class="col-3">Student5</div>
+        <div class="col-3">25</div>
+        <div class="col-3">4500</div>
+        <div class="col-3">333</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
