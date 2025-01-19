f0d008dc6108f007f8f25a31bcba9c9122810316
Mattew-Kentor <matveyzhikharev@yandex.ru>
2025-01-20 02:19:47 +0300

    add current student events

--- src/views/StudentsPage.vue
+++ src/views/StudentsPage.vue
@@ -6,6 +6,7 @@ export default {
     return {
       students: [],
       studentEmail: '',
+      events: []
     }
   },
   methods: {
@@ -38,6 +39,15 @@ export default {
       }).then(response => {
         this.loadStudents()
       })
+    },
+    async getTable(studentId) {
+      await axios.get('/mystudent/' + studentId + '/events', {
+        headers: {
+          'Authorization': `Bearer ${localStorage.accessToken}`
+        }
+      }).then(response => {
+        this.loadStudents()
+      })
     }
   },
   mounted() {
@@ -47,31 +57,56 @@ export default {
 </script>
 
 <template>
-  <div class="container my-5" data-v-inspector="src/views/StudentsPage.vue:50:3">
-    <h1 class="mb-4" data-v-inspector="src/views/StudentsPage.vue:51:5">Список учеников</h1>
-    <form class="mb-4" data-v-inspector="src/views/StudentsPage.vue:52:5">
-      <input
-          v-model="studentEmail"
-          type="email"
-          class="form-control"
-          placeholder="Введите почту" data-v-inspector="src/views/StudentsPage.vue:53:7">
-      <button
-          class="btn btn-primary"
-          @click="addStudent" data-v-inspector="src/views/StudentsPage.vue:58:7">Добавить ученика</button>
-    </form>
-    <div class="d-flex flex-column gap-3" data-v-inspector="src/views/StudentsPage.vue:62:5">
-      <div class="row fw-bold" data-v-inspector="src/views/StudentsPage.vue:63:7">
-        <div class="col-3" data-v-inspector="src/views/StudentsPage.vue:64:9">Почта</div>
-        <div class="col-3" data-v-inspector="src/views/StudentsPage.vue:65:9">Имя</div>
-        <div class="col-3" data-v-inspector="src/views/StudentsPage.vue:66:9">Фамилия</div>
-        <div class="col-3" data-v-inspector="src/views/StudentsPage.vue:67:9">Дополнительная информация</div>
+  <div class="container my-5">
+    <div class="d-flex flex-column gap-3">
+      <h2 class="mb-4">Расписание выбранного пользователя</h2>
+      <div v-for="event in events">
+        <div class="row fw-bold">
+          <div class="col-2">Организатор</div>
+          <div class="col-2">Название</div>
+          <div class="col-4">Описание</div>
+          <div class="col-2">Гость</div>
+          <div class="col-2">Дата</div>
+        </div>
+        <div class="row">
+          <div class="col-2">{{ event.user.email }}</div>
+          <div class="col-2">{{ event.name }}</div>
+          <div class="col-4">{{ event.description }}</div>
+          <div class="col-2">{{ event.folowed_user.email }}</div>
+          <div class="col-2">{{ event.date_created }}</div>
+        </div>
       </div>
-      <div class="row" v-for="student in students" data-v-inspector="src/views/StudentsPage.vue:69:7">{{}}
-        <div class="col-2" data-v-inspector="src/views/StudentsPage.vue:70:9">{{student.user.email}}</div>
-        <div class="col-2" data-v-inspector="src/views/StudentsPage.vue:71:9">{{student.user.firstName}}</div>
-        <div class="col-2" data-v-inspector="src/views/StudentsPage.vue:72:9">{{student.user.lastName}}</div>
-        <div class="col-4" data-v-inspector="src/views/StudentsPage.vue:73:9">{{student.user.extraInfo}}</div>
-        <div class="col-2" @click="deleteStudent(student.studentId)" data-v-inspector="src/views/StudentsPage.vue:74:9">Удалить</div>
+    </div>
+    <h2 class="mb-4">Список учеников</h2>
+    <form class="mb-4 row">
+      <div class="col-9">
+        <input
+            v-model="studentEmail"
+            type="email"
+            class="form-control"
+            placeholder="Введите почту">
+      </div>
+      <button
+          class="btn btn-secondary col-3"
+          @click="addStudent" data-v-inspector="src/views/StudentsPage.vue:58:7">Добавить ученика
+      </button>
+    </form>
+    <div class="d-flex flex-column gap-3">
+      <div class="row fw-bold">
+        <div class="col-2">Почта</div>
+        <div class="col-2">Имя</div>
+        <div class="col-2">Фамилия</div>
+        <div class="col-4">Дополнительная информация</div>
+      </div>
+      <div class="row" v-for="student in students">
+        <div class="col-2">{{ student.user.email }}</div>
+        <div class="col-2">{{ student.user.firstName }}</div>
+        <div class="col-2">{{ student.user.lastName }}</div>
+        <div class="col-4">{{ student.user.extraInfo }}</div>
+        <div class="col-2">
+          <div class="text-danger" @click="deleteStudent(student.studentId)">Удалить</div>
+          <div class="text-success" @click="getTable(student.studentId)">Открыть расписание</div>
+        </div>
       </div>
     </div>
   </div>
