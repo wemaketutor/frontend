3630727802faa623f1267ea2b5681db7ac756394
Mattew-Kentor <matveyzhikharev@yandex.ru>
2025-01-20 03:06:20 +0300

    profile update

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -20,34 +20,31 @@ export default {
 </script>
 
 <template>
-  <header class="p-3 text-bg-dark" data-v-inspector="src/components/AppHeader.vue:23:3">
-    <div class="container" data-v-inspector="src/components/AppHeader.vue:24:5">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start"
-           data-v-inspector="src/components/AppHeader.vue:25:7">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
-           data-v-inspector="src/components/AppHeader.vue:26:9">
-          <div class="logo d-flex align-items-center" data-v-inspector="src/components/AppHeader.vue:27:11">
+  <header class="p-3 text-bg-dark">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <div class="logo d-flex align-items-center">
             <img
                 src="/src/assets/logo-white.svg"
                 class="logo-img"
-                width="50px" data-v-inspector="src/components/AppHeader.vue:28:13">
-            <span class="fs-4" data-v-inspector="src/components/AppHeader.vue:32:13">Tutoring assistant</span>
+                width="50px">
+            <span class="fs-4">Tutoring assistant</span>
           </div>
         </a>
 
-        <ul class="nav col-12 col-lg-auto me-lg-auto ms-lg-auto mb-2 justify-content-center mb-md-0 gap-5"
-            data-v-inspector="src/components/AppHeader.vue:36:9">
-          <li data-v-inspector="src/components/AppHeader.vue:37:11">
+        <ul class="nav col-12 col-lg-auto me-lg-auto ms-lg-auto mb-2 justify-content-center mb-md-0 gap-5">
+          <li>
             <a
                 class="nav-link px-2"
                 @click="goPage('main')"
                 :class="{
                   'text-secondary': $route.name === 'main',
                   'text-white': $route.name !== 'main'
-                }" data-v-inspector="src/components/AppHeader.vue:38:13"
+                }"
             >Главная</a>
           </li>
-          <li data-v-inspector="src/components/AppHeader.vue:47:11">
+          <li>
             <a
                 v-if="this.$cookies.get('role') === 'ROLE_TEACHER'"
                 class="nav-link px-2"
@@ -55,7 +52,7 @@ export default {
                 :class="{
                   'text-secondary': $route.name === 'students',
                   'text-white': $route.name !== 'students'
-                }" data-v-inspector="src/components/AppHeader.vue:48:13"
+                }"
             >Ученики</a>
             <a
                 v-else
@@ -64,35 +61,35 @@ export default {
                 :class="{
                   'text-secondary': $route.name === 'teachers',
                   'text-white': $route.name !== 'teachers'
-                }" data-v-inspector="src/components/AppHeader.vue:57:13"
+                }"
             >Учителя</a>
           </li>
-          <li data-v-inspector="src/components/AppHeader.vue:67:11">
+          <li>
             <a
                 class="nav-link px-2"
                 @click="goPage('resources')"
                 :class="{
                   'text-secondary': $route.name === 'resources',
                   'text-white': $route.name !== 'resources'
-                }" data-v-inspector="src/components/AppHeader.vue:68:13"
+                }"
             >Материалы</a>
           </li>
         </ul>
 
-        <div class="text-end" v-if="!email" data-v-inspector="src/components/AppHeader.vue:79:9">
+        <div class="text-end" v-if="!email">
           <button
               type="button"
               class="btn btn-outline-light me-2"
-              @click="goPage('login')" data-v-inspector="src/components/AppHeader.vue:80:11">Войти
+              @click="goPage('login')">Войти
           </button>
           <button
               type="button"
               class="btn btn-secondary"
-              @click="goPage('register')" data-v-inspector="src/components/AppHeader.vue:85:11">Зарегистрироваться
+              @click="goPage('register')">Зарегистрироваться
           </button>
         </div>
-        <div v-else data-v-inspector="src/components/AppHeader.vue:91:9">
-          <span data-v-inspector="src/components/AppHeader.vue:92:11">{{ email }}</span>
+        <div v-else>
+          <a @click="goPage('profile')">{{ email }}</a>
         </div>
       </div>
     </div>
--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,9 +1,85 @@
-<script setup>
+<script>
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      user: {
+        email: '',
+        firstName: '',
+        lastName: '',
+        avatar: '',
+        extra_info: '',
+      }
+    }
+  },
+  methods: {
+    async loadProfile() {
+      await axios.get('/profile', {
+        headers: {
+          'Authorization': `Bearer ${localStorage.accessToken}`
+        }
+      }).then(response => {
+        this.user = response.data.user;
+      })
+    },
+    async updateProfile() {
+      await axios.put('/profile/update', {
+        avatar: this.user.avatar,
+        firstName: this.user.firstName,
+        lastName: this.user.lastName,
+        extraInfo: this.user.extraInfo,
+      }, {
+        headers: {
+          'Authorization': `Bearer ${localStorage.accessToken}`
+        }
+      })
+    },
+  },
+  mounted() {
+    this.loadProfile()
+  }
+}
 </script>
 
 <template>
-
+  <div class="container mt-5">
+    <h2 class="mb-4">Профиль пользователя</h2>
+    <div class="mb-3">
+      <label for="avatar" class="form-label fw-bold">Аватар:</label>
+      <img v-if="user.avatar" :src="user.avatar" alt="аватар" width="256px">
+      <input type="url" id="avatar" class="form-control" :value="user.avatar">
+    </div>
+    <div class="mb-3">
+      <label for="email" class="form-label fw-bold">Email:</label>
+      <input type="email" id="email" class="form-control" :value="user.email" disabled>
+    </div>
+    <div class="mb-3">
+      <label for="firstName" class="form-label fw-bold">Имя:</label>
+      <input type="text" id="firstName" class="form-control" :value="user.firstName">
+    </div>
+    <div class="mb-3">
+      <label for="lastName" class="form-label fw-bold">Фамилия:</label>
+      <input type="text" id="lastName" class="form-control" :value="user.lastName">
+    </div>
+    <div class="mb-3">
+      <label for="role" class="form-label fw-bold">Роль:</label>
+      <select
+          v-model="user.role"
+          class="form-select"
+          id="role"
+          required
+          disabled>
+        <option value="ROLE_STUDENT">Ученик</option>
+        <option value="ROLE_TEACHER">Учитель</option>
+      </select>
+    </div>
+    <div class="mb-3">
+      <label for="additionalInfo" class="form-label fw-bold">Дополнительная информация:</label>
+      <textarea id="additionalInfo" class="form-control" rows="3" v-model="user.extra_info"></textarea>
+    </div>
+    <button class="btn btn-secondary" @click="updateProfile">Обновить профиль</button>
+  </div>
 </template>
 
 <style scoped>
